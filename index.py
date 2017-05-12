fe52adc8b3df35310ca25be552943d01bd3b79e5
huongsterha <huongsterha@gmai.com>
2017-05-12 10:36:07 -0400

    Added ansible, two pages, and prometheus stuff

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from prometheus_metrics import setup_metrics
 
 app = Flask(__name__)
+setup_metrics(app)
 
 @app.route('/')
 def main():
     return render_template('main.html')
+    
+@app.route('/Gear')
+def coffee():
+    return render_template('gear.html')
+    
+@app.route('/Gear/Shoes')
+def linktocoffee():
+    return render_template('shoes.html')
 
 if __name__== '__main__':
     app.run(debug=True,host='0.0.0.0',port=8080)
--- index_test.py
+++ index_test.py
@@ -1,40 +1,25 @@
 import unittest
-
-
-
 import index
-
-
-
-
 
 class FlaskrTestCase(unittest.TestCase):
 
-
-
     def setUp(self):
-
         self.app = index.app.test_client()
 
-
-
     def tearDown(self):
-
         pass
 
-
-
     def test_home_page(self):
-
-        # Render the / path of the website
-
-        rv = self.app.get('/')
-
-        # Chech that the page contians the desired phrase
-
+	    rv = self.app.get('/')
         assert b'Rock Climbing' in rv.data
+        
+    def test_link_to_my_page(self):
+        rv = self.app.get('/Gear')  
+        assert b'Shoes' in rv.data 
 
-
+    def test_my_topic(self):
+        rv = self.app.get('/Gear/Shoes')  
+        assert b'La Sportiva' in rv.data 
 
 if __name__ == '__main__':
 
